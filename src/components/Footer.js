6f125ebf283b95db6ef56cfbbecc9c0a5739037d
Brandon Franks <bgfranks106@icloud.com>
2019-11-21 20:52:56 -0500

    moved the footer css to styled-components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "./Footer.css";
+import styled from "styled-components";
+
+const FooterContainer = styled.div`
+  background: #333333;
+  color: #f3f3f3;
+  height: 13vh;
+`;
+
+const FooterPara = styled.p`
+  text-align: center;
+  margin: 0;
+  padding-top: 20px;
+`;
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <FooterContainer>
       <footer>
-        <p>Page created by Brandon Franks 2019</p>
-        <p>NASA logo is a register trademark of NASA 2019</p>
+        <FooterPara>Page created by Brandon Franks 2019</FooterPara>
+        <FooterPara>NASA logo is a register trademark of NASA 2019</FooterPara>
       </footer>
-    </div>
+    </FooterContainer>
   );
 };
 
