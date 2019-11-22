f4388b95c17a917b02477658ed4ba175381e532b
Brandon Franks <bgfranks106@icloud.com>
2019-11-21 21:02:04 -0500

    moved header css to sytled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,45 @@
 import React from "react";
 import logo from "../img/nasa_logo.jpeg";
 import "./Header.css";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px 30px;
+  border-bottom: 1px solid #0b3d91;
+`;
+
+const LogoImg = styled.img`
+  height: 85px;
+  width: auto;
+`;
+
+const NavItem = styled.a`
+  padding: 0 20px;
+  font-size: 1.2rem;
+  text-decoration: none;
+  color: #0b3d91;
+  transition: 0.3s;
+  &:hover {
+    font-size: 1.3rem;
+  }
+`;
 
 const Header = () => {
   return (
-    <div className="header">
+    <HeaderContainer>
       <div className="logo">
-        <img src={logo} alt="Nasa Logo" />
+        <LogoImg src={logo} alt="Nasa Logo" />
       </div>
       <div className="nav">
-        <a className="nav-item" href="#">
-          Home
-        </a>
-        <a className="nav-item" href="#">
-          About
-        </a>
-        <a className="nav-item" href="#">
-          Media
-        </a>
-        <a className="nav-item" href="#">
-          Contact
-        </a>
+        <NavItem src="#">Home</NavItem>
+        <NavItem src="#">About</NavItem>
+        <NavItem src="#">Media</NavItem>
+        <NavItem src="#">Contact</NavItem>
       </div>
-    </div>
+    </HeaderContainer>
   );
 };
 
