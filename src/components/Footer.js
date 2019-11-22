642a3b05784d334665c462877fc5e9686cd1c55e
Brandon Franks <bgfranks106@icloud.com>
2019-11-21 23:04:29 -0500

    fixed a typo in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,9 @@ const Footer = () => {
     <FooterContainer>
       <footer>
         <FooterPara>Page created by Brandon Franks 2019</FooterPara>
-        <FooterPara>NASA logo is a register trademark of NASA 2019</FooterPara>
+        <FooterPara>
+          NASA logo is a registered trademark of NASA 2019
+        </FooterPara>
       </footer>
     </FooterContainer>
   );
